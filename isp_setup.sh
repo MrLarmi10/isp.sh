6c6998e61367f979722e2008e125b797e955ed9c
MrLarmi10 <vladkucenko7602@gmail.com>
2026-04-23 12:58:26 +0300

    Add files via upload

---
+++ isp_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ===== ISP ROUTER =====
+hostnamectl set-hostname isp.au-team.irpo
+
+# Включение маршрутизации
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+# DHCP на внешнем интерфейсе
+nmcli con mod ens160 ipv4.method auto
+nmcli con up ens160
+
+# Интерфейс к HQ
+nmcli con mod ens192 ipv4.addresses 172.168.1.1/27 ipv4.method manual
+nmcli con up ens192
+
+# Интерфейс к BR
+nmcli con mod ens224 ipv4.addresses 172.168.2.1/27 ipv4.method manual
+nmcli con up ens224
+
+# NAT
+cat > /etc/nftables/isp.nft <<EOF
+table inet nat {
+ chain POSTROUTING {
+  type nat hook postrouting priority srcnat;
+  oifname "ens160" masquerade
+ }
+}
+EOF
+
+echo 'include "/etc/nftables/isp.nft"' >> /etc/sysconfig/nftables.conf
+systemctl enable --now nftables
+
+# Timezone
+timedatectl set-timezone Europe/Moscow
+
+echo "ISP configured"
